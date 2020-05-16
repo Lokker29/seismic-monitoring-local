5a57a499eb6eb34cf16dca7713d26d97f457da9e
Bohdan Bilokon <bohdan.bilokon@nure.ua>
2020-05-16 20:05:20 +0300

    add avg by country

--- gui/gui.py
+++ gui/gui.py
@@ -1,6 +1,9 @@
-from datetime import date
+from datetime import date, datetime
 
 import pygame
+import pygame_gui
+from dateutil.relativedelta import relativedelta
+
 from colors_constant import web_colors as colors
 from config import MONGO_DB_NAME, MONGO_DB_HOST, MONGO_DB_USER, MONGO_DB_PASSWORD, BASE_SEISMIC_COLLECTION
 from db import DBClient
@@ -65,7 +68,7 @@ def start_app(width=1000, height=600):
         btn_position = (btn_x_start, btn_y_start, btn_width, btn_height)
 
         display_btn(display, mouse, btn_position, colors['blackmagic'], colors['knightsarmor'], 'state_statistic',
-                    mouse_click_down, 'Количество сейсмических активностей по странам/штатам за период',
+                    mouse_click_down, 'Количество сейсмических активностей по странам/штатам c 10-03-2020',
                     colors['white'], 20, btn_x_start + btn_width / 2, btn_y_start + btn_height / 2,
                     client=client, splot=splot)
 
@@ -84,9 +87,9 @@ def start_app(width=1000, height=600):
         btn_position = (btn_x_start, btn_y_start, btn_width, btn_height)
 
         display_btn(display, mouse, btn_position, colors['blackmagic'], colors['knightsarmor'], 'avg_mag',
-                    mouse_click_down, 'Средняя магнитуда на планете по дням за период',
+                    mouse_click_down, 'Средняя магнитуда по дням (за последние 50 дней) по странам/штатам',
                     colors['white'], 20, btn_x_start + btn_width / 2, btn_y_start + btn_height / 2,
-                    client=client, splot=splot)
+                    client=client, splot=splot, width=width, height=height)
         ####
         count += 1
         btn_y_start = height / division * count
@@ -117,11 +120,13 @@ def handler_btn(display, mouse, pos, color_before, color_during, action=None, mo
 
     action_dict = {
         'state_statistic': count_of_state,
-        'avg_mag': avg_mag,
+        # 'avg_mag': avg_mag,
+        'avg_mag': select_country,
         'ai_today_mag': ai_today_mag,
         'return_to_main': start_app,
         'quit': handle_quit,
         'show_map': show_map,
+        'country_avg': country_avg,
     }
 
     if pos[0] + pos[2] > mouse[0] > pos[0] and pos[1] + pos[3] > mouse[1] > pos[1]:
@@ -191,3 +196,92 @@ def display_btn(display, mouse, pos, color_before, color_during, action, clicked
 
     handler_btn(display, mouse, pos, color_before, color_during, action, clicked, **kwargs)
     display_text(display, text, text_color, font_size, text_x_center, text_y_center)
+
+
+def select_country(client, splot, start=datetime(2020, 3, 10), end=datetime.today(), **kwargs):
+    data = client.get_data_by_range_date(start, end, 'place', 'coord', 'time')
+
+    width = kwargs['width']
+    height = kwargs['height']
+
+    display = pygame.display.set_mode((width, height))
+
+    clock = pygame.time.Clock()
+
+    manager = pygame_gui.UIManager((800, 600))
+
+    all_countries = []
+    for row in data:
+        state = row['place'].split(', ')[-1]
+        all_countries.append(state)
+    all_countries = list(sorted(set(all_countries)))
+
+    default_text = '---'
+    drop_down_menu = pygame_gui.elements.UIDropDownMenu(
+        all_countries, starting_option=default_text, manager=manager,
+        relative_rect=pygame.Rect((width / 8 * 2, height / 6 * 2), (width / 2, height / 12))
+    )
+
+    active = True
+    while active:
+        time_delta = clock.tick(30) / 1000.0
+        events = pygame.event.get()
+        events_type = [event.type for event in events]
+
+        quit_was_happen = pygame.QUIT in events_type
+        if quit_was_happen:
+            handle_quit()
+
+        for event in events:
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+                    if event.ui_element == drop_down_menu:
+                        default_text = event.text
+
+            manager.process_events(event)
+
+        manager.update(time_delta)
+
+        mouse_click_down = pygame.MOUSEBUTTONDOWN in events_type
+
+        display.fill(colors['bisque'])
+
+        text = ['Выберите страну (или штат), чтобы отобразить', 'последние сейсмические активности в стране/штате']
+        for num, elem in enumerate(text):
+            display_text(display, elem, colors['black'], 30, width / 2, height / 6 + num * 50)
+
+        mouse = pygame.mouse.get_pos()
+
+        btn_x_start, btn_y_start = width / 10, height / 7 * 5
+        btn_width, btn_height = width / 6 * 5, height / 12
+
+        btn_position = (btn_x_start, btn_y_start, btn_width, btn_height)
+
+        display_btn(display, mouse, btn_position, colors['blackmagic'], colors['knightsarmor'], 'country_avg',
+                    mouse_click_down, 'Отобразить средние значения магнитуды по дням', colors['white'], 20,
+                    btn_x_start + btn_width / 2, btn_y_start + btn_height / 2,
+                    client=client, splot=splot, country_name=default_text)
+
+        btn_y_start = height / 7 * 6
+        btn_position = (btn_x_start, btn_y_start, btn_width, btn_height)
+
+        display_btn(display, mouse, btn_position, colors['blackmagic'], colors['knightsarmor'], 'return_to_main',
+                    mouse_click_down, 'Вернуться на начало', colors['white'], 20,
+                    btn_x_start + btn_width / 2, btn_y_start + btn_height / 2, width=width, height=height)
+
+        manager.draw_ui(display)
+        pygame.display.update()
+
+
+def country_avg(client, splot, country_name):
+    if country_name != '---':
+        start = datetime.today() - relativedelta(days=50)
+        end = datetime.today()
+        data = client.get_data_by_range_date(start, end, 'time', 'mag', 'place')
+
+        new_data = []
+        for row in data:
+            if row['place'].endswith(country_name):
+                new_data.append(row)
+
+        avg_mag(client, splot, data=new_data)
--- gui/handlers.py
+++ gui/handlers.py
@@ -8,11 +8,14 @@ def count_of_state(client, splot, start=datetime.today() - relativedelta(days=50
     splot.plot_statistic_of_states(data)
 
 
-def avg_mag(client, splot, start=datetime.today() - relativedelta(days=50), end=datetime.today()):
-    data = client.get_data_by_range_date(start, end, 'time', 'mag')
+def avg_mag(client, splot, start=datetime.today() - relativedelta(days=50), end=datetime.today(), data=None):
+    if data is None:
+        data = client.get_data_by_range_date(start, end, 'time', 'mag')
     splot.plot_mean_magnitude_by_date(data)
 
 
-def show_map(client, splot, start=datetime.today() - relativedelta(days=50), end=datetime.today()):
-    data = client.get_data_by_range_date(start, end, 'place', 'coord', 'time')
+def show_map(client, splot, start=datetime.today() - relativedelta(days=50), end=datetime.today(), data=None):
+    if data is None:
+        data = client.get_data_by_range_date(start, end, 'place', 'coord', 'time')
+
     splot.show_map(data)
